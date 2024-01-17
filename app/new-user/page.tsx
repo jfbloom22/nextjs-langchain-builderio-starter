e280871eae9eca8d10904416de680526e7925ae0
Jonathan Flower <jfbloom22@gmail.com>
2024-01-17 11:30:14 -0500

    creating new user in db from clerk authenticated user

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,5 +1,30 @@
-const NewUser = () => {
-    return <div>New User</div>
+import { currentUser } from "@clerk/nextjs";
+import { prisma } from "../utils/db"
+import {redirect} from "next/navigation"
+const createNewUser = async () => {
+    const user = await currentUser();
+    if (!user) {
+        return <div>Not logged in</div>
+    }
+    const match = await prisma.user.findUnique({
+        where: {
+            clerkId: user.id
+        }
+    })
+    if (!match) {
+        const newUser = await prisma.user.create({
+            data: {
+                clerkId: user.id,
+                email: user.emailAddresses[0].emailAddress,
+            }
+        })
+    }
+
+redirect('/journal')
+}
+const NewUser = async () => {
+    await createNewUser()
+    return <div>...loading</div>
 }
 
 export default NewUser
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,25 @@
+import { auth } from "@clerk/nextjs";
 import Link from "next/link";
 
-export default function Home() {
+
+export default async function Home() {
+  const {userId} = await auth();
+
+  let href = "/new-user"
+
+  if (userId) {
+    href = "/journal"
+  }
+
   return (
 <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
   <div className="w-full max-w-[400w] mx-auto">
     <h1 className="text-6xl font-bold mb-4">Hello World</h1>
     <p className="text-2xl text-white/60 mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.</p>
     <div>
-      <Link href="/journal">
+      <Link href={href}>
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Button
+        Get Started
       </button>
       </Link>
     </div>
